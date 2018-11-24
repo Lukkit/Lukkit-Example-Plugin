32446bd98f3dc143cea9c1e049a1a7c522b2638b
James Upjohn <jspartan250@gmail.com>
2018-11-24 15:10:00 +1300

    Wrote a basic plugin

---
+++ Basic Plugin/main.lua
@@ -0,0 +1,7 @@
+plugin.onEnable(function
+    logger.info("Enabling Basic Plugin v1.0.0! (Lukkit-Example-Plugin)")
+end)
+
+plugin.onDisable(function
+    logger.info("Disable Basic Plugin v1.0.0! (Lukkit-Example-Plugin)")
+end)
